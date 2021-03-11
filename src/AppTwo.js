46833e967a6a48bb43031f39ccbf0e06c0cfb59a
kccrtv <kimcincocreative@gmail.com>
2021-03-10 20:16:55 -0800

    scaffold and style hero page mockup

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,6 +1,11 @@
 import './styles.css';
 import phonetop from './components/assets/Header.svg';
-import phonebottom from './components/assets/bottom-footer.svg';
+// import phonebottom from './components/assets/bottom-footer.svg';
+import users from './components/assets/users-solid.svg';
+import gradcap from './components/assets/gradcap-solid.svg';
+import home from './components/assets/home-solid.svg';
+import faves from './components/assets/fire-wave-solid.svg';
+import more from './components/assets/ellipses-solid.svg';
 
 function AppTwo() {
 	return (
@@ -17,8 +22,8 @@ function AppTwo() {
 					</figure>
 
 					{/* Middle Horizontal Scroll */}
-					<h4 className='thumbnail-header'>Header</h4>
-					<div className='thumbnail-section'>
+					<h4 className='thumbnail-header first-header'>Header</h4>
+					<div className='thumbnail-section first-section'>
 						<ul className='thumbs'>
 							<li className='thumb'>
 								<h5>1</h5>
@@ -35,27 +40,52 @@ function AppTwo() {
 						</ul>
 					</div>
 					{/* Another Middle Horizontal Scroll */}
-					<h4 className='thumbnail-header'>Header</h4>
-					<div className='thumbnail-section'>
+					<h4 className='thumbnail-header second-header'>Header</h4>
+					<div className='thumbnail-section second-section'>
 						<ul className='thumbs'>
 							<li className='thumb'>
-								<h5>1</h5>
+								<h5>5</h5>
 							</li>
 							<li className='thumb'>
-								<h5>2</h5>
+								<h5>6</h5>
 							</li>
 							<li className='thumb'>
-								<h5>3</h5>
+								<h5>7</h5>
 							</li>
 							<li className='thumb'>
-								<h5>4</h5>
+								<h5>8</h5>
 							</li>
 						</ul>
 					</div>
 
 					{/* Bottom */}
+					<div className='navbar-container'>
+						<div className='icon-container'>
+							<input className='icon' type='image' src={users} alt='icon' />
+							<p className='icon-text'>Community</p>
+						</div>
+						<div className='icon-container'>
+							<input className='icon' type='image' src={gradcap} alt='icon' />
+							<p className='icon-text'>Skills</p>
+						</div>
+						<div className='icon-container'>
+							<input className='icon' type='image' src={home} alt='icon' />
+							<p className='icon-text'>Home</p>
+						</div>
+						<div className='icon-container'>
+							<input className='icon' type='image' src={faves} alt='icon' />
+							<p className='icon-text'>Favorites</p>
+						</div>
+						<div className='icon-container'>
+							<input className='icon' type='image' src={more} alt='icon' />
+							<p className='icon-text'>More</p>
+						</div>
+					</div>
 					<section className='bottom-nav'>
-						<img src={phonebottom} alt='phone base'></img>
+						{/* <img
+							className='phone-bottom'
+							src={phonebottom}
+							alt='phone base'></img> */}
 					</section>
 				</div>
 			</main>
@@ -68,4 +98,5 @@ export default AppTwo;
 /* resources: https://www.w3schools.com/cssref/css_units.asp
  *https://developer.mozilla.org/en-US/docs/Web/CSS/repeat()
 https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_hor_scroll
+ https://stackoverflow.com/questions/8683528/embed-image-in-a-button-element
 */
